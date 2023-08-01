26227efa9efd7ebbdd4e6bb7b105546df443cb72
leonardogiagio <leonardogiagio@gmail.com>
2023-08-01 01:11:15 -0300

    add: cookies

---
+++ 03_funcoes_parametros_includes/17_cookies.php
@@ -0,0 +1,49 @@
+<?php
+    if(isset($_COOKIE['nome'])) {
+        echo '<h2>' . $_COOKIE['nome'] . '</h2>';
+    }
+?>
+
+<form method="GET">
+    <p>
+        <label for="">
+            Nome: <br>
+            <input type="text" name="nome">
+        </label>
+    </p>
+
+    <p>
+        <label for="">
+            Idade: <br>
+            <input type="text" name="idade">
+        </label>
+    </p>
+
+    <p>
+        <label for="">
+            email: <br>
+            <input type="email" name="email">
+        </label>
+    </p>
+
+    <input type="submit" value="Enviar">
+
+</form>
+
+<?php
+
+$nome = filter_input(INPUT_GET, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
+$idade = filter_input(INPUT_GET, 'idade', FILTER_VALIDATE_INT);
+$email = filter_input(INPUT_GET, 'email', FILTER_VALIDATE_EMAIL);
+
+if($nome && $email && $idade) {
+
+    $expiracao = time() + (86400 * 30);
+    setcookie('nome', $nome, $expiracao);
+
+    echo 'Nome: ' . $nome . '<br>';
+    echo 'Idade: ' . $idade . '<br>';
+    echo 'email: ' . $email . '<br>';
+} else {
+    echo 'Não enviou';
+}
