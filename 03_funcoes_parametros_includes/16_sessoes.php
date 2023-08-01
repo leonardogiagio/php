c9affa89f9fc27a941fe0e459c2ed0a412b2c3da
leonardogiagio <leonardogiagio@gmail.com>
2023-08-01 01:11:05 -0300

    add: sessoes

---
+++ 03_funcoes_parametros_includes/16_sessoes.php
@@ -0,0 +1,49 @@
+<?php
+    session_start();
+
+    if($_SESSION['aviso']) {
+        echo $_SESSION['aviso'];
+        $_SESSION['aviso'] = '';
+    }
+?>
+
+
+<form method="GET">
+    <p>
+        <label for="">
+            Nome: <br>
+            <input type="text" name="nome">
+        </label>
+    </p>
+
+    <p>
+        <label for="">
+            Idade: <br>
+            <input type="text" name="idade">
+        </label>
+    </p>
+
+    <p>
+        <label for="">
+            email: <br>
+            <input type="email" name="email">
+        </label>
+    </p>
+
+    <input type="submit" value="Enviar">
+
+</form>
+
+<?php
+
+$nome = filter_input(INPUT_GET, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
+$idade = filter_input(INPUT_GET, 'idade', FILTER_VALIDATE_INT);
+$email = filter_input(INPUT_GET, 'email', FILTER_VALIDATE_EMAIL);
+
+if($nome && $email && $idade) {
+    echo 'Nome: ' . $nome . '<br>';
+    echo 'Idade: ' . $idade . '<br>';
+    echo 'email: ' . $email . '<br>';
+} else {
+    $_SESSION['aviso'] = 'Preencha os itens corretamente';
+}
