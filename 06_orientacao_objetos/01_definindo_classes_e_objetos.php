4f37396d840586486f85eff5d133c02678148696
leonardogiagio <leonardogiagio@gmail.com>
2023-08-27 00:51:39 -0300

    add: encapsulamento

--- 06_orientacao_objetos/01_definindo_classes_e_objetos.php
+++ 06_orientacao_objetos/01_definindo_classes_e_objetos.php
@@ -2,13 +2,31 @@
 
 class Post {
     // propriedades
-    public int $likes = 0;
-    public array $comments = [];
-    public string $author;
+    private int $likes = 0;
+    private array $comments = [];
+    private string $author;
 
     // metodo construtor
     public function __construct(int $qtLikes) {
         $this->likes = $qtLikes;
+    }
+
+
+    // encapsulamento
+    public function setAuthor($author) {
+        $this->author = ucfirst($author);
+    }
+
+    public function getAuthor() {
+        return $this->author ?? 'Visitante';
+    }
+
+    public function setLikes($like) {
+        $this->likes = $like;
+    }
+
+    public function getLikes() {
+        return $this->likes;
     }
 
     // metodos
@@ -17,6 +35,8 @@ class Post {
     }
 }
 
+// criando objeto
 $post = new Post(10);
 $post->aumentarLike();
-echo $post->likes;
+$post->setAuthor('Leonardo');
+echo 'Esse post tem ' . $post->getLikes() . ' likes e o autor é o ' . $post->getAuthor();
