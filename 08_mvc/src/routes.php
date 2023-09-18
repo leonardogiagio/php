eedc344b9d2f4d0ed2fa24d63b71ddec0d6d5561
leonardogiagio <leonardogiagio@gmail.com>
2023-09-18 00:21:31 -0300

    add: criando rotas do create

--- 08_mvc/src/routes.php
+++ 08_mvc/src/routes.php
@@ -4,7 +4,5 @@ use core\Router;
 $router = new Router();
 
 $router->get('/', 'HomeController@index');
-$router->get('/sobre/{nome}', 'HomeController@sobreP');
-$router->get('/sobre', 'HomeController@sobre');
-$router->get('/foto/{id}', 'HomeController@foto');
-$router->get('/fotos', 'HomeController@fotos');
+$router->get('/novo', 'UsuariosController@add');
+$router->post('/novo', 'UsuariosController@addAction');
